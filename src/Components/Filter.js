62273f54f0174919fee2628a6327c9a323e49767
phvinayak017 <vinayak.hulabutti@gmail.com>
2019-09-12 14:02:17 +0530

    2nd commit

---
+++ src/Components/Filter.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+
+export default class Filter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            bedSelectedOption: 'isAllBeds'
+        }
+    }
+
+    handleChange = (e) => {
+        const { value: key, checked: value } = e.target
+        console.log(key, value)
+        this.setState({
+            bedSelectedOption: key
+        })
+        this.props.setFilter(key, value)
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h2>Beds</h2>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isAllBeds"
+                            checked={this.state.bedSelectedOption === 'isAllBeds'}
+                            onChange={this.handleChange}
+                        /> All Beds ( )
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isOneBed"
+                            checked={this.state.bedSelectedOption === 'isOneBed'}
+                            onChange={this.handleChange}
+                        /> 1 Bed ( )
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isTwoBed"
+                            checked={this.state.bedSelectedOption === 'isTwoBed'}
+                            onChange={this.handleChange}
+                        /> 2 Beds ( )
+                    </label>
+                </div><br />
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isStudio"
+                            checked={this.state.bedSelectedOption === 'isStudio'}
+                            onChange={this.handleChange}
+                        />Studio ( )
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isThreeBed"
+                            checked={this.state.bedSelectedOption === 'isThreeBed'}
+                            onChange={this.handleChange}
+                        /> 3 Beds ( )
+                    </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value="isFourPlusBed"
+                            checked={this.state.bedSelectedOption === 'isFourPlusBed'}
+                            onChange={this.handleChange}
+                        /> 4+ Beds ( )
+                    </label>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+import Filter from './Filter'
+
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            propertyData: [],
+            catCount: 0,
+            dogCount: 0,
+            bedCount: 0,
+            bathCount: 0,
+            unitCount: 0,
+            hookupCount: 0,
+            onsiteCount: 0,
+            filters: {
+                isOneBed: false,
+                isTwoBed: false,
+                isThreeBed: false,
+                iFourPlusBed: false,
+                isStudio: false,
+                isAllBeds: false,
+                isOnePlusBaths: false,
+                isTwoPlusBaths: false,
+                isThreePlusBaths: false,
+                isAllBath: false,
+                isDogs: false,
+                isCats: false,
+                isBothPets: false,
+                isApartments: false,
+                isCondos: false,
+                isHouse: false,
+                isTowmhomes: false,
+                isInUnit: false,
+                isHookups: false,
+                isOnSite: false,
+            }
+
+        }
+    }
+
+    setFilter = (key, value) => {
+        return ({ ...this.state.filters, [key]: value })
+    }
+
+    componentDidMount() {
+        const url = `https://sandoratest-service.herokuapp.com/api/property/quickView?long=
+        -121.88632860000001&lat=37.3382082&distance=100&userId=null`
+        Axios({
+            url,
+            method: "get",
+            auth: {
+                username: 'testuser',
+                password: 'testpassword'
+            },
+        })
+            .then(({ data: { data } }) => {
+                this.setState({
+                    propertyData: data
+                })
+            })
+    }
+
+    handleClick = () => {
+        console.log(this.state.propertyData)
+    }
+
+    render() {
+        // console.log(this.state.filters)
+        return (
+            <div>
+                <div className='navbar'>
+                    <div className="dropdown">
+                        <button className='btnfilter'>Filter</button>
+                        <div className="dropdown-content">
+                            <div>
+                                <Filter
+                                    filters={this.state.filters}
+                                    setFilter={this.setFilter}
+                                />
+                            </div>
+                            <div className="filter-bottom ">
+                                <button className="btn-clear">Clear</button>
+                                <button className='btn-done'>Done</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='mainarea'>
+                    {/* <button
+                        onClick={this.handleClick}
+                    >Get Data</button> */}
+                </div>
+
+            </div>
+        )
+    }
+}
